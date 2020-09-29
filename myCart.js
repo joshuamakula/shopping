739e8c9680965e9b9908b698eb61b4d0bb862487
Joshua Makula <makulajosh@gmail.com>
2020-09-29 08:24:20 +0300

    first commit

---
+++ myCart.js
@@ -0,0 +1,13 @@
+var cart = ["Hp EliteBook", "Dell", "Viao", "Toshiba"];
+
+for(let item = 0; item <= cart.length; item++){
+    // console.log(cart[item]);
+}
+
+// Add to cart
+/* cart.push("Dell Celeron");
+console.log(cart);
+ */
+// Remove from cart
+cart.splice(0,1)
+console.log(cart);
